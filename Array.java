0bc2de8c53a4669fbec17524650105ccab7fecda
satish-nirmal <satishnirmal16@gmail.com>
2023-03-07 00:51:07 +0530

    this is for the practice

---
+++ Array.java
@@ -0,0 +1,14 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		
+		int a[4]= {10,20,30,40};
+		
+		for (int i=0 ;, i<a.length;i++){
+
+        System.out.println(a[i]);
+
+		}
+	}
+}
