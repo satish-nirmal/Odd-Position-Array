1adf60e4835ecb0a69247a445642220b8b51d6a7
satish-nirmal <satishnirmal16@gmail.com>
2023-03-08 23:26:00 +0530

    program for the best practice

---
+++ EvenArray.java
@@ -0,0 +1,19 @@
+
+public class EvenArray {
+
+	public static void main(String[] args) {
+		int[] a= {1,2,3,4,5,6};
+	
+		System.out.println("even position elemment");
+		
+		for(int i=1;i<a.length;i=i+2) {
+
+			System.out.print(a[i]+" ");
+			
+			
+			
+		}
+	
+	}
+
+}
